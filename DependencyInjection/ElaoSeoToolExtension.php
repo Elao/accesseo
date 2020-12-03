ad396312c535dcb1b532037bd7f37c3239e20208
Anne-Laure de Boissieu <al.deboissieu@elao.com>
2020-12-03 15:53:57 +0100

    declare service config

---
+++ DependencyInjection/ElaoSeoToolExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Elao\Bundle\SEOTool\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class ElaoSeoToolExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.xml');
+    }
+}
