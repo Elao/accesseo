2e8f357a6189ab9800aed76568ba0a6d4589dc0b
Anne-Laure de Boissieu <al.deboissieu@elao.com>
2020-12-31 16:45:14 +0100

    wip

--- src/Controller/BrokenLinkController.php
+++ src/Controller/BrokenLinkController.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace Elao\Bundle\SeoTool\Controller;
 
+use Elao\Bundle\SeoTool\Checker\BrokenLinkChecker;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-class BrokenLinkController extends AbstractController
+class BrokenLinkController
 {
-    public function getBrokenLinks(): Response
+    public function __invoke(): Response
     {
-        return $this->json();
+        return new JsonResponse(
+            [
+                '200' => ['http://www.google.com', 'https://www.nasa.gov/'],
+                '404' => ['http://www.google.com/error'],
+                'invalid' => ['http://www.google/error'],
+                'timeout' => ['http://www.google/timeout']
+            ]
+        );
     }
 }
